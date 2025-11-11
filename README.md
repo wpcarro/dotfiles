d5282430c2929ad1dd1fa48a59f547364d2e20e7
William Carroll <wpcarro@gmail.com>
2025-11-11 15:51:47 -0800

    feat(bashrc): Multiple installation options
    This workflow is slowly killing me:
    
    ```shell
    $ billsh
    $ ssh ops@foundation
    $ billsh
    $ echo here
    ```
    
    So I decided to play around with Nix Profiles and see if I could
    replace `billsh` with a simpler workflow (see README.md)

--- README.md
+++ README.md
@@ -4,9 +4,32 @@
 
 ## Install
 
+You have a few installation options:
+1. Use Nix Profiles (recommended)
+2. Overwrite existing `~/.bashrc`
+3. Standalone executable: `billsh`
+
+Use Nix Profiles (recommended)
+
+```shell
+$ nix-env -f https://github.com/wpcarro/dotfiles/archive/main.tar.gz -iA dotfiles.bashrc
+$ vim ~/.bashrc # add "source ~/.nix-profile/etc/bashrc"
+```
+
+Overwrite existing `~/.bashrc`
+
+```shell
+$ mv ~/.bashrc{,.bak} # backup your bashrc
+$ ln -sf $(nix-build https://github.com/wpcarro/dotfiles/archive/main.tar.gz -A dotfiles.bashrc --no-out-link)/etc/bashrc ~/.bashrc
+```
+
+Standalone executable: `billsh`
+
 ```shell
 $ nix-env -f https://github.com/wpcarro/dotfiles/archive/main.tar.gz -iA dotfiles.shell
 ```
+
+## Nix
 
 You may need to install Nix:
 
--- default.nix
+++ default.nix
@@ -65,64 +65,71 @@ let
       magenta = wrap 35;
       cyan    = wrap 36;
     };
-    bashrc = pkgs.writeText "bashrc" ''
-      export PATH=${self.path}:${if true then "/opt/homebrew/bin:$PATH" else "$PATH"}
+    # NOTE: You can install this with:
+    #   $ nix-env -f path/to/dotfiles -iA dotfiles.bashrc
+    #   $ vim ~/.bashrc # add `source ~/nix-profile/etc/bashrc`
+    bashrc = pkgs.writeTextFile {
+      name = "bashrc";
+      destination = "/etc/bashrc";
+      text = ''
+        export PATH=${self.path}:${if true then "/opt/homebrew/bin:$PATH" else "$PATH"}
 
-      alias m='cd ~/programming/matrix'
-      alias base='cd ~/programming/base'
+        alias m='cd ~/programming/matrix'
+        alias base='cd ~/programming/base'
 
-      alias depot='cd ~/programming/depot'
-      alias dotfiles='cd ~/programming/dotfiles'
+        alias depot='cd ~/programming/depot'
+        alias dotfiles='cd ~/programming/dotfiles'
 
-      alias eb='vim ~/programming/dotfiles/default.nix'
-      alias sb='nix-env -f ~/programming/dotfiles -iA dotfiles.shell && exec billsh'
-      alias la='ls -al'
-      alias rgh='rg --hidden'
-      alias tpr='tput reset'
+        alias eb='vim ~/programming/dotfiles/default.nix'
+        alias sb='nix-env -f ~/programming/dotfiles -iA dotfiles.shell && exec billsh'
+        alias la='ls -al'
+        alias rgh='rg --hidden'
+        alias tpr='tput reset'
 
-      alias gst='git status'
-      alias gd='git diff'
-      alias gco='git checkout'
-      alias gcb='git checkout -b'
-      alias gprom='git pull --rebase origin main'
-      alias gfrom='git rebase -i --autosquash origin/main'
-      alias grbc='git rebase --continue'
-      alias grh='git reset --hard'
-      alias glp='git log --graph --oneline --decorate-refs=r/ --decorate-refs=refs/remotes/origin/'
-      alias gpf='git push --force'
-      alias gcan='git commit --amend --no-edit --no-verify'
-      alias gca='git commit --amend --no-verify'
-      alias gcf='git commit --fixup --no-verify'
-      alias gcp='git cherry-pick'
-      alias gds='git diff --staged'
-      alias gsh='git show HEAD'
-      alias gc='git commit'
-      alias gp='git push'
-      alias gpr='gh pr create --title="$(git log -1 --format=%s)" --body="$(git log -1 --format=%b)"'
+        alias gst='git status'
+        alias gd='git diff'
+        alias gco='git checkout'
+        alias gcb='git checkout -b'
+        alias gprom='git pull --rebase origin main'
+        alias gfrom='git rebase -i --autosquash origin/main'
+        alias grbc='git rebase --continue'
+        alias grh='git reset --hard'
+        alias glp='git log --graph --oneline --decorate-refs=r/ --decorate-refs=refs/remotes/origin/'
+        alias gpf='git push --force'
+        alias gcan='git commit --amend --no-edit --no-verify'
+        alias gca='git commit --amend --no-verify'
+        alias gcf='git commit --fixup --no-verify'
+        alias gcp='git cherry-pick'
+        alias gds='git diff --staged'
+        alias gsh='git show HEAD'
+        alias gc='git commit'
+        alias gp='git push'
+        alias gpr='gh pr create --title="$(git log -1 --format=%s)" --body="$(git log -1 --format=%b)"'
 
-      alias vim='simple_vim'
-      export EDITOR=${depot.users.wpcarro.tools.simple_vim}/bin/simple_vim
-      export GIT_EDITOR=$EDITOR
+        alias vim='simple_vim'
+        export EDITOR=${depot.users.wpcarro.tools.simple_vim}/bin/simple_vim
+        export GIT_EDITOR=$EDITOR
 
-      # apps
-      eval "$(direnv hook bash)"
-      source $HOME/.nix-profile/etc/profile.d/nix.sh
-      source ${pkgs.bash-preexec}/share/bash/bash-preexec.sh
-      eval "$(atuin init bash)"
+        # apps
+        eval "$(direnv hook bash)"
+        source $HOME/.nix-profile/etc/profile.d/nix.sh
+        source ${pkgs.bash-preexec}/share/bash/bash-preexec.sh
+        eval "$(atuin init bash)"
 
-      # sensitive cleartext
-      [ -f $HOME/secrets.sh ] && source $HOME/secrets.sh
+        # sensitive cleartext
+        [ -f $HOME/secrets.sh ] && source $HOME/secrets.sh
 
-      # prompt
-      fmt_git_branch() {
-        x=$(git branch --show-current 2>/dev/null)
-        [ -n "$x" ] && printf "[%s]" "$x"
-      }
-      export PS1='${self.colors.red "["}${self.colors.blue "\\u"}${self.colors.yellow "@"}${self.colors.magenta "\\h"}${self.colors.red "]"} ${self.colors.cyan "\\w"} $(fmt_git_branch)\n\t ${self.colors.yellow "λ"} '
-    '';
+        # prompt
+        fmt_git_branch() {
+          x=$(git branch --show-current 2>/dev/null)
+          [ -n "$x" ] && printf "[%s]" "$x"
+        }
+        export PS1='${self.colors.red "["}${self.colors.blue "\\u"}${self.colors.yellow "@"}${self.colors.magenta "\\h"}${self.colors.red "]"} ${self.colors.cyan "\\w"} $(fmt_git_branch)\n\t ${self.colors.yellow "λ"} '
+      '';
+    };
 
     shell = pkgs.writeShellScriptBin "billsh" ''
-      exec ${pkgs.bashInteractive}/bin/bash --rcfile ${self.bashrc}
+      exec ${pkgs.bashInteractive}/bin/bash --rcfile ${self.bashrc}/etc/bashrc
     '';
   });
 in
